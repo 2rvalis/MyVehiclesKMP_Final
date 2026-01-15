c7eb037027af4287da1ab6257a9372db37569369
Evangelos <tourvalis@gmail.com>
2026-01-15 21:31:34 +0200

    Format workflow and add clean flag

--- composeApp/src/iosMain/kotlin/com/example/myvehicles/DatabaseBuilder.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/myvehicles/DatabaseBuilder.ios.kt
@@ -2,13 +2,13 @@ package com.example.myvehicles
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import platform.Foundation.NSHomeDirectory
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 
-actual fun getDatabaseBuilder(): RoomDatabase.Builder<VehicleDatabase> {
-    val dbFilePath = NSHomeDirectory() + "/vehicles.db"
-    return Room.databaseBuilder<VehicleDatabase>(
+fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
+    val dbFilePath = NSHomeDirectory() + "/my_vehicles.db"
+    return Room.databaseBuilder<AppDatabase>(
         name = dbFilePath,
-        factory = { VehicleDatabase::class.instantiateImpl() }
+        factory = { AppDatabase::class.instantiateImpl() } // Εδώ είναι το κλειδί
     ).setDriver(BundledSQLiteDriver())
 }
--- composeApp/src/iosMain/kotlin/com/example/myvehicles/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/myvehicles/MainViewController.kt
@@ -2,4 +2,11 @@ package com.example.myvehicles
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    // Δημιουργούμε το factory εδώ για το iOS
+    val database = getDatabaseBuilder().build()
+    val repository = VehicleRepository(database.vehicleDao())
+    val factory = VehicleViewModelFactory(repository)
+
+    App(viewModelFactory = factory)
+}
