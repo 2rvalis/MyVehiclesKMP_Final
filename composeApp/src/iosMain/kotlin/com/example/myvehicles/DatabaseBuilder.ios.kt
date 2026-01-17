b54c96b5052a1abc799ecda8cf6f6348c00bfe14
Evangelos <tourvalis@gmail.com>
2026-01-17 07:57:47 +0200

    Attempt alternative factory instantiation for iOS

--- composeApp/src/iosMain/kotlin/com/example/myvehicles/DatabaseBuilder.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/myvehicles/DatabaseBuilder.ios.kt
@@ -4,12 +4,18 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
 
-// Η λέξη 'actual' είναι ΥΠΟΧΡΕΩΤΙΚΗ εδώ
+@OptIn(ExperimentalForeignApi::class)
 actual fun getDatabaseBuilder(): RoomDatabase.Builder<VehicleDatabase> {
     val dbFilePath = NSHomeDirectory() + "/my_vehicles.db"
     return Room.databaseBuilder<VehicleDatabase>(
         name = dbFilePath,
-        factory = { VehicleDatabase::class.instantiateImpl() }
+        factory = { instantiateImpl() } // Χωρίς το VehicleDatabase::class μπροστά
     ).setDriver(BundledSQLiteDriver())
+}
+
+// Αυτό το "κόλπο" βοηθάει τον compiler του iOS να βρει τη γεννημένη κλάση
+private fun instantiateImpl(): VehicleDatabase {
+    return VehicleDatabase::class.instantiateImpl()
 }
