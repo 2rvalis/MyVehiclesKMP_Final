44cfc8f817a2324c8992258dd5be34f9f8ebb7f7
Evangelos <tourvalis@gmail.com>
2026-01-18 07:35:10 +0200

    Change VehicleDatabase to abstract class and remove android actual

--- composeApp/src/commonMain/kotlin/com/example/myvehicles/VehicleDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/example/myvehicles/VehicleDatabase.kt
@@ -7,11 +7,10 @@ import androidx.room.RoomDatabaseConstructor
 
 @Database(entities = [Vehicle::class], version = 3, exportSchema = false)
 @ConstructedBy(AppDatabaseConstructor::class)
-// Προσθήκη κενού constructor για να βοηθήσουμε το generation
-expect abstract class VehicleDatabase : RoomDatabase {
+abstract class VehicleDatabase : RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
 }
 
-// Marker interface για τον compiler
+// Μόνο ο Constructor παραμένει expect
 @Suppress("NO_ACTUAL_FOR_EXPECT")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<VehicleDatabase>
